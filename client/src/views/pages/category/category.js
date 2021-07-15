ad632b664333ba036f5091aeb4798034202b179e
Kyun Heo <kyun2dot@gmail.com>
2021-07-16 01:19:09 +0900

    chore : 카테고리 페이지 css 수정 및 목업 데이터 추가

--- client/src/views/pages/category/category.js
+++ client/src/views/pages/category/category.js
@@ -1,15 +1,35 @@
+import categoryListItem from '../../components/categoryListItem';
+import styles from './category.css';
+
 const category = {
   render: async () => {
-    const view = `<div class="page category-page">카테고리 페이지<button class="back-btn">뒤로가기</button></div>`;
+    const categoryData = [
+      { img: 'src/mockup/image.png', title: '디지털기기' },
+      { img: 'src/mockup/image.png', title: '생활가전' },
+      { img: 'src/mockup/image.png', title: '가구/인테리어' },
+      { img: 'src/mockup/image.png', title: '게임/취미' },
+      { img: 'src/mockup/image.png', title: '생활/가공식품' },
+      { img: 'src/mockup/image.png', title: '스포츠/레저' },
+      { img: 'src/mockup/image.png', title: '여성패션/잡화' },
+      { img: 'src/mockup/image.png', title: '남성패션/잡화' },
+      { img: 'src/mockup/image.png', title: '유아동' },
+      { img: 'src/mockup/image.png', title: '뷰티/미용' },
+      { img: 'src/mockup/image.png', title: '반려동물' },
+      { img: 'src/mockup/image.png', title: '도서/티켓/음반' },
+    ];
+
+    let categoryItems = '';
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of categoryData) {
+      // eslint-disable-next-line no-await-in-loop
+      categoryItems += await categoryListItem.render(item);
+    }
+
+    const view = `<div class="page category-page"><div class="${styles['category-container']}">${categoryItems}</div></div>`;
 
     return view;
   },
-  afterRender: async () => {
-    const backBtn = document.querySelector('.back-btn');
-    backBtn.addEventListener('click', () => {
-      window.history.back();
-    });
-  },
+  afterRender: async () => {},
 };
 
 export default category;
