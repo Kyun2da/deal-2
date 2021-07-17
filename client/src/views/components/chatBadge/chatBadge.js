5b898425bab4e113d45de448f2aeb26531ed5085
CurlyBat <82671736+CurlyBat@users.noreply.github.com>
2021-07-17 16:45:00 +0900

    변경 : chat-badge 컴포넌트의 클래스 이름 통합

--- client/src/views/components/chatBadge/chatBadge.js
+++ client/src/views/components/chatBadge/chatBadge.js
@@ -2,16 +2,7 @@ import './chatBadge.css';
 
 const chatBadge = {
   render: async (count) => {
-    let classname = '';
-    if (count < 10) {
-      classname = 'single-digit';
-    } else if (count < 100) {
-      classname = 'ten-digits';
-    } else {
-      classname = 'hundred-digits';
-    }
-
-    const view = `<div class="chat-badge ${classname}">${count}</div>`;
+    const view = `<div class="chat-badge count">${count}</div>`;
 
     return view;
   },
