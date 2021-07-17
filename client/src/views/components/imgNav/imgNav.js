3a65db9203579b1b36f4dea5d58d7448974d30cd
Kyun Heo <kyun2dot@gmail.com>
2021-07-17 14:59:17 +0900

    추가 : imgNav 컴포넌트 추가

---
+++ client/src/views/components/imgNav/imgNav.js
@@ -0,0 +1,15 @@
+import './imgNav.css';
+
+const imgNav = {
+  render: async (isActive = false, idx) => {
+    const classname = isActive ? 'active' : 'inactive';
+    const view = `
+    <button class="img-nav ${classname}" name="${idx}"></button>
+    `;
+    return view;
+  },
+
+  after_render: async () => {},
+};
+
+export default imgNav;
---
+++ client/src/views/components/imgNav/index.js
@@ -0,0 +1,3 @@
+import imgNav from './imgNav';
+
+export default imgNav;
