26021b97f0f3c152210d001e9f2612a4aa4d8f50
Kyun Heo <kyun2dot@gmail.com>
2021-07-17 17:51:32 +0900

    chore : imgNav 컴포넌트 비활성화일때는 클래스명 없게 변경

--- client/src/views/components/imgNav/imgNav.js
+++ client/src/views/components/imgNav/imgNav.js
@@ -2,7 +2,7 @@ import './imgNav.css';
 
 const imgNav = {
   render: async (isActive = false, idx) => {
-    const classname = isActive ? 'active' : 'inactive';
+    const classname = isActive ? 'active' : '';
     const view = `
     <button class="img-nav ${classname}" name="${idx}"></button>
     `;
