c532a08df400b0426ef1327ccee04101b7d9745b
CurlyBat <82671736+CurlyBat@users.noreply.github.com>
2021-07-19 00:54:38 +0900

    추가 : inputPopup 컴포넌트 레이아웃

---
+++ client/src/views/components/modal/inputPopup.js
@@ -0,0 +1,37 @@
+import './inputPopup.css';
+import textInput from '../textInput';
+import changeConfirmButton from '../../../services/changeConfirmButton';
+
+const inputPopup = {
+  render: async () => {
+    const popupMessage = '현재 위치를 입력하세요.';
+    const cancelMessage = '취소';
+    const confirmMessage = '확인';
+    const inputTown = await textInput.render(
+      'medium-input',
+      '시·구 제외, 동만 입력'
+    );
+    const view = `
+        <div class="input-popup">
+          <p class="popup-message">
+            ${popupMessage}
+          </p>
+          ${inputTown}
+          <div class="popup-buttons">
+            <Button class="cancel-button">
+              ${cancelMessage}
+            </Button>
+            <Button class="confirm-button">
+              ${confirmMessage}
+            </Button>
+          </div>
+        </div>
+    `;
+
+    return view;
+  },
+
+  afterRender: async () => {
+};
+
+export default inputPopup;
