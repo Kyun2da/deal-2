9bef73ee3acf1ccd5d7a1c7aff07cdb500d1de1f
CurlyBat <82671736+CurlyBat@users.noreply.github.com>
2021-07-19 20:54:28 +0900

    추가 : 회원가입시 input 값 검증 issue #23

---
+++ client/src/services/checkSignup.js
@@ -0,0 +1,12 @@
+const checkSignup = (area) => {
+  const idInput = document.getElementsByName('id')[0];
+  const townInput = document.getElementsByName('town')[0];
+  const idRegex = /[0-9a-zA-Z]{5,20}/;
+  const townRegex = /동$/;
+  const button = area.getElementsByTagName('Button')[0];
+  if (idRegex.test(idInput.value) && townRegex.test(townInput.value))
+    button.disabled = false;
+  else button.disabled = true;
+};
+
+export default checkSignup;
