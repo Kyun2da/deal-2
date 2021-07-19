dc17983c6dbcf0146379f1c46c14f23ebc18f0e0
CurlyBat <82671736+CurlyBat@users.noreply.github.com>
2021-07-19 17:48:35 +0900

    추가 : 로그인 페이지 로그인 페이지 추가에 따른 변경사항들도 함께 적용

--- client/src/views/pages/login/login.js
+++ client/src/views/pages/login/login.js
@@ -1,14 +1,37 @@
 import icon from '../../components/icon';
+import menuHeader from '../../components/menuHeader';
+import textInput from '../../components/textInput';
+import { button } from '../../components/button';
 import './login.css';
 
 const login = {
   render: async () => {
-    const categoryIcon = await icon.render(
-      'src/images/category.svg',
-      '카테고리',
-      'category-icon'
+    const backIcon = await icon.render(
+      'src/images/chevron-left.svg',
+      '뒤로 가기'
     );
-    const view = `<div class="login-page">${categoryIcon}</div>`;
+    const menuHeaderItem = await menuHeader.render(
+      '#/',
+      backIcon,
+      null,
+      '로그인',
+      'off-white'
+    );
+    const idInput = await textInput.render(
+      'large-input',
+      '아이디를 입력하세요'
+    );
+    const loginButton = await button.render('button-large', '로그인');
+    const view = `<div class="page login">
+                    ${menuHeaderItem}
+                    <form class="login-area">
+                      ${idInput}
+                      ${loginButton}
+                      <a href="#/signup" class="signup-button">
+                        회원가입
+                      </a>
+                    </form>
+                  </div>`;
 
     return view;
   },
