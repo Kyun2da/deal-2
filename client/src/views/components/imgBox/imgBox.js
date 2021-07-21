c8390277b537dfe96fc08a33f19bbfa1e9e5defe
Kyun Heo <kyun2dot@gmail.com>
2021-07-22 00:57:31 +0900

    수정 : 큰따옴표 없었던 것 수정
    Co-Authored-By: CurlyBat <82671736+CurlyBat@users.noreply.github.com>

--- client/src/views/components/imgBox/imgBox.js
+++ client/src/views/components/imgBox/imgBox.js
@@ -2,7 +2,7 @@ import './imgBox.css';
 
 const imgBox = {
   render: async (src = '', alt = '', classname = '') => {
-    const view = `<div><img src=${src} alt=${alt} class="img-box ${classname}" /></div>`;
+    const view = `<div><img src="${src}" alt="${alt}" class="img-box ${classname}" /></div>`;
     return view;
   },
 
