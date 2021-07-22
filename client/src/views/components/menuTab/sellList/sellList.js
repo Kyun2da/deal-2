827f230c9b991366d78196cc892b2d76d1a23515
Kyun Heo <kyun2dot@gmail.com>
2021-07-22 20:41:45 +0900

    추가 : 판매 목록 api 받아서 렌더링 및 이벤트 리스너 달기

--- client/src/views/components/menuTab/sellList/sellList.js
+++ client/src/views/components/menuTab/sellList/sellList.js
@@ -1,27 +1,46 @@
-import data from '../../../../mockup/sellList.json';
+import api from '../../../../apis';
+import productOnClick from '../../../../services/common/productOnClick';
+import utils from '../../../../services/common/utils';
 import productListItem from '../../productListItem/productListItem';
 
 const sellList = {
   render: async () => {
     let sellListComponent = ``;
-    for (const item of data) {
-      const { img, title, town, locTime, price, isLike, chatCount, likeCount } =
-        item;
-      sellListComponent += await productListItem.render(
-        true,
-        img,
-        title,
-        town,
-        locTime,
-        price,
-        isLike,
-        chatCount,
-        likeCount
-      );
+    const id = localStorage.getItem('id');
+    if (id) {
+      const { result } = await api.get(`/selllist/${id}`);
+      for (const item of result) {
+        const {
+          idx,
+          sellerId,
+          image,
+          title,
+          town,
+          createdAt,
+          price,
+          likeNum,
+          chatNum,
+        } = item;
+
+        sellListComponent += await productListItem.render(
+          idx,
+          id === sellerId,
+          image,
+          title,
+          town,
+          utils.calcDate(createdAt),
+          price,
+          false,
+          chatNum,
+          likeNum
+        );
+      }
+    } else {
+      sellListComponent += '로그인이 필요합니다!';
     }
 
     const view = `<div class="page">
-                    <div>
+                    <div class="product-list">
                       ${sellListComponent}
                     </div>
                   </div>
@@ -29,7 +48,13 @@ const sellList = {
 
     return view;
   },
-  afterRender: async () => {},
+  afterRender: async () => {
+    const productContainer = document.querySelector('.menu .product-list');
+    const { children } = productContainer;
+    Array.from(children).forEach((item) => {
+      productOnClick(item);
+    });
+  },
 };
 
 export default sellList;
