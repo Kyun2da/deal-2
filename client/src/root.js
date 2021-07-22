94ecfa2fae3b877a16f58f64575f29a10c08b02c
Kyun Heo <kyun2dot@gmail.com>
2021-07-22 12:21:44 +0900

    chore : 쓸데없는 주석 제거 및 url파싱 방법 변경

--- client/src/root.js
+++ client/src/root.js
@@ -33,27 +33,17 @@ const mainpage = ['/'];
 const leftPages = ['/category'];
 const rightPages = ['/login', '/menu'];
 
-// The router code. Takes a URL, checks against the list of supported routes and then renders the corresponding content page.
 const router = async (e) => {
-  // Lazy load view element:
   const content = null || document.getElementById('slide');
 
-  // Get the parsed URl from the addressbar
-  const request = utils.parseRequestURL();
+  const parsedURL = utils.parseHostURL() || '/';
 
-  // Parse the URL and if it has an id part, change it with the string ":id"
-  const parsedURL =
-    (request.resource ? `/${request.resource}` : '/') +
-    (request.id ? '/:id' : '') +
-    (request.verb ? `/${request.verb}` : '');
-
-  // Get the page from our hash of supported routes.
-  // If the parsed URL is not in our list of supported routes, select the 404 page instead
   const page = routes[parsedURL] ? routes[parsedURL] : error404;
   const pageWidth = 435;
   const emptyPrevElement = `<div class="empty prev"></div>`;
   const emptyNextElement = `<div class="empty next"></div>`;
   const currentPageUrl = window.location.hash.slice(1).toLowerCase() || '/';
+
   if (e.type === 'hashchange') {
     const backPageUrl = e.oldURL.split('#')[1] || '/';
     // leftPages에 있는 url로 들어갈 시
@@ -114,7 +104,6 @@ const router = async (e) => {
       slideAnimation(content.lastChild, -pageWidth);
     } else content.innerHTML += emptyNextElement;
   }
-  // https://hi-dot.tistory.com/8
 
   await page.afterRender();
 };
