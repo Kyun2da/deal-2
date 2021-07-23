06b8e60fe02724e2e20ea656fb26a030c361fa92
Kyun Heo <kyun2dot@gmail.com>
2021-07-23 12:00:36 +0900

    추가 : product delete api 서버 추가

--- server/product/product.controller.js
+++ server/product/product.controller.js
@@ -3,6 +3,7 @@ const {
   selectProduct,
   insertProduct,
   selectDetailProduct,
+  deleteDetailProduct,
 } = require('./product.service');
 
 const router = express.Router();
@@ -26,6 +27,13 @@ router.post('/', async (req, res) => {
   if (success)
     res.status(200).send({ msg: '성공적으로 상품을 등록하였습니다.' });
   else res.status(500).send('server error');
+});
+
+router.delete('/:id', async (req, res) => {
+  const { success, result } = await deleteDetailProduct(req.params.id);
+  if (success) {
+    res.status(200).send({ result });
+  } else res.status(500).send('server error');
 });
 
 module.exports = router;
